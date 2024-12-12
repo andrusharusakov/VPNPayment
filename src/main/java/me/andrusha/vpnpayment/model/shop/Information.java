6619420af86dcc2fbda7d6b8df4b547295a8cc8a
Andrew <fylhtqhecfrjd@gmail.com>
2024-12-12 21:00:37 +0300

    Чучуть фиксим

--- src/main/java/me/andrusha/vpnpayment/model/shop/Information.java
+++ src/main/java/me/andrusha/vpnpayment/model/shop/Information.java
@@ -1,17 +1,13 @@
 package me.andrusha.vpnpayment.model.shop;
 
 import javax.persistence.Embeddable;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Embeddable
 public class Information {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private String name;
     private String description;
 
+    // Геттеры и сеттеры
     public String getName() {
         return name;
     }
@@ -26,5 +22,5 @@ public class Information {
 
     public void setDescription(String description) {
         this.description = description;
-        }
+    }
 }
