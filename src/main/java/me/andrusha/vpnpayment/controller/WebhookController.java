cb1c7b6fbc56407b5a87b659b4ca84e931610f83
Andrew <fylhtqhecfrjd@gmail.com>
2024-12-14 00:51:19 +0300

    Фикс

--- src/main/java/me/andrusha/vpnpayment/controller/WebhookController.java
+++ src/main/java/me/andrusha/vpnpayment/controller/WebhookController.java
@@ -1,6 +1,5 @@
 package me.andrusha.vpnpayment.controller;
 
-import me.andrusha.vpnpayment.model.payment.PromoPayment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
